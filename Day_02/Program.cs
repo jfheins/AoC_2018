26ca59a6cd38814a6b8e24e59b90a02969879d17
Julius F. Heins <jfheins@gmail.com>
2018-12-02 06:17:52 +0100

    Day 2, Part 1

--- Day_02/Program.cs
+++ Day_02/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day_02
 {
@@ -6,7 +9,19 @@ namespace Day_02
 	{
 		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			var input = new [] { "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" };
+			input = File.ReadAllLines(@"../../../input.txt");
+
+			var BoxIdList = input.Select(id => id
+				.ToLookup(c => c)
+				.Select(chargroup => chargroup.Count())
+				.Where(count => count > 1)
+				).Select(group => new HashSet<int>(group)).ToList();
+
+			var checksum = BoxIdList.Count(boxid => boxid.Contains(2)) * BoxIdList.Count(boxid => boxid.Contains(3));
+
+			Console.WriteLine($"Checksum für this list is {checksum}");
+			Console.ReadLine();
 		}
 	}
 }
