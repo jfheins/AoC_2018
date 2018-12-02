83a5b16328aca7786ef93fc00e59353886b4dd1c
Julius F. Heins <jfheins@gmail.com>
2018-12-02 06:41:32 +0100

    Prep

---
+++ Day_03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Day_03
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var input = File.ReadAllLines(@"../../../input.txt");
+
+
+			Console.ReadLine();
+		}
+	}
+}
