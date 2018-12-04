a27cce007d5877bdc70489dd8f07f681b8712ac5
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-04 09:07:19 +0000

    Small refactoring of Day 3

---
+++ Day_03/ClaimIntersector.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Core.Combinatorics;
+
+namespace Day_03
+{
+    class ClaimIntersector
+    {
+        public List<Claim> Claims { get; }
+
+        private ClaimIntersector(IEnumerable<Claim> claims)
+        {
+            Claims = claims.ToList();
+        }
+
+        public static ClaimIntersector Parse(IEnumerable<string> input)
+        {
+            return new ClaimIntersector(input.Select(ParseClaim));
+        }
+
+        public int GetIntersectionArea()
+        {
+            var maxX = Claims.Max(c => c.Area.Right);
+            var maxY = Claims.Max(c => c.Area.Bottom);
+            int intersectionArea = 0;
+            for (int x = 0; x < maxX; x++)
+            {
+                for (int y = 0; y < maxY; y++)
+                {
+                    var intersection = Claims.Where(claim => claim.Area.Contains(x, y)).ToArray();
+
+                    if (intersection.Length > 1)
+                    {
+                        intersectionArea++;
+                    }
+                }
+            }
+
+            return intersectionArea;
+        }
+
+        public IEnumerable<int> GetNonIntersectingClaims()
+        {
+            var claimIds = new HashSet<int>(Claims.Select(c => c.Id));
+            foreach (var pair in new Combinations<Claim>(Claims, 2))
+            {
+                if (pair[0].Area.IntersectsWith(pair[1].Area))
+                {
+                    claimIds.Remove(pair[0].Id);
+                    claimIds.Remove(pair[1].Id);
+                }
+            }
+
+            return claimIds;
+        }
+
+        private static Claim ParseClaim(string line)
+        {
+            // Format: #1 @ 1,3: 4x4
+            var regex = new Regex(@"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)");
+
+            var groups = regex.Match(line).Groups;
+            Debug.Assert(groups.Count == 6);
+            var numbers = groups.Skip(1).Select(g => int.Parse(g.Value)).ToArray();
+            return new Claim(numbers[0], new Rectangle(numbers[1], numbers[2], numbers[3], numbers[4]));
+        }
+
+        public struct Claim
+        {
+            public Claim(int id, Rectangle area)
+            {
+                Id = id;
+                Area = area;
+            }
+
+            public Rectangle Area { get; set; }
+            public int Id { get; set; }
+        }
+    }
+}
--- Day_03/Program.cs
+++ Day_03/Program.cs
@@ -13,55 +13,29 @@ namespace Day_03
 	{
 		private static void Main(string[] args)
 		{
+            var sw = new Stopwatch();
+            sw.Start();
 			var input = File.ReadAllLines(@"../../../input.txt");
 		    //input = new [] {"#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"};
 
-		    var claimsById = input.Select(ParseClaim).ToDictionary(t => t.id, t => t.claim);
-		    var claims = claimsById.Values.ToList();
-		    var allIds = new HashSet<int>(claimsById.Keys);
+		    var solver = ClaimIntersector.Parse(input);
 
-
-            var maxX = claims.Max(c => c.Right);
-		    var maxY = claims.Max(c => c.Bottom);
-
-		    int intersectionArea = 0;
-		    for (int x = 0; x < maxX; x++)
-		    {
-		        for (int y = 0; y < maxY; y++)
-		        {
-		            var intersection = claimsById.Where(claim => claim.Value.Contains(x, y)).ToArray();
-
-                    if (intersection.Length > 1)
-		            {
-                        // Intersection !
-		                intersectionArea++;
-		                foreach (var intersectingClaim in intersection)
-		                {
-		                    allIds.Remove(intersectingClaim.Key);
-		                }
-                    }
-		        }
-		    }
+		    var intersectionArea = solver.GetIntersectionArea();
+		    var remaining = solver.GetNonIntersectingClaims().ToList();
+		    
 
 		    Console.WriteLine(intersectionArea);
-		    foreach (var remaining in allIds)
+		    Console.WriteLine();
+
+		    foreach (var claim in remaining)
 		    {
-		        Console.WriteLine($"Non-intersecting: #{remaining}");
+		        Console.WriteLine($"Non-intersecting: #{claim}");
 		    }
+            sw.Stop();
+		    Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms.");
             Console.ReadLine();
 		}
 
 
-	    private static  (int id, Rectangle claim) ParseClaim(string line)
-	    {
-	        // Format: #1 @ 1,3: 4x4
-	        var regex = new Regex(@"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)");
-
-	        var groups = regex.Match(line).Groups;
-	        Debug.Assert(groups.Count == 6);
-	        var numbers = groups.Skip(1).Select(g => int.Parse(g.Value)).ToArray();
-	        var id = numbers[0];
-	        return (id, new Rectangle(numbers[1], numbers[2], numbers[3], numbers[4]));
-	    }
 	}
 }
