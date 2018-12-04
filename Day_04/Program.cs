9657ee7a1f824ac074e93f7ae1851f694989d7b7
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-04 09:08:55 +0000

    Started Day 4

---
+++ Day_04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
