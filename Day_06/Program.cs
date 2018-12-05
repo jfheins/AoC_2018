2540b03261f65690a06eccb0ab04d2ab2734bdef
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-05 17:32:11 +0000

    Prep

---
+++ Day_06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day_06
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = File.ReadAllText(@"../../../input.txt").Trim();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            
+
+            sw.Stop();
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms.");
+
+            Console.ReadLine();
+        }
+    }
+}
