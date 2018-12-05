3a1f0a656cf36ead08527101179e66398bdc4358
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-05 06:08:06 +0000

    First try

--- Day_05/Program.cs
+++ Day_05/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day_05
 {
@@ -7,10 +9,31 @@ namespace Day_05
     {
         private static void Main(string[] args)
         {
-            var input = File.ReadAllLines(@"../../../input.txt");
+            var input = File.ReadAllText(@"../../../input.txt");
+            input = "dabAcCaCBAcCcaDA";
 
+            var result = ReduceAllPairs(input);
+            Console.WriteLine(result.Length);
 
             Console.ReadLine();
+        }
+
+        private static string ReduceAllPairs(string input)
+        {
+            var output = new Stack<char>(input.Length);
+            foreach (var letter in input)
+            {
+                if (output.TryPeek(out var prev) && DoReact(prev, letter))
+                    output.Pop();
+                else
+                    output.Push(letter);
+            }
+            return string.Concat(output.Reverse());
+        }
+
+        private static bool DoReact(char a, char b)
+        {
+            return a != b && a.ToString().Equals(b.ToString(), StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
