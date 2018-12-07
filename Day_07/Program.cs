b2f3a6d8229f7f50d9b7de74fe426cd2b33ba5f4
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-07 05:49:10 +0000

    Day 7

---
+++ Day_07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"../../../input.txt");
+
+            
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+            sw.Stop();
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms.");
+
+            Console.ReadLine();
+        }
+    }
+}
