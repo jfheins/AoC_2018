99fbe2ae5969460094bb0977d67cd28971b9da66
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-07 19:57:25 +0000

    Prep Day 8

---
+++ Day_08/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Core;
+
+namespace Day_08
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
