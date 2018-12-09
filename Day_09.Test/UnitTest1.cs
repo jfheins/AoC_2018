0b47a6f97ff1ab27c507055bd74b3a852633b3b1
Julius F. Heins <jfheins@gmail.com>
2018-12-09 20:11:06 +0100

    First Test red

---
+++ Day_09.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day_09.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Test0()
+		{
+			var game = new MarbleGame();
+			Assert.AreEqual("[-] (0)", game.ToString());
+		}
+
+
+		[TestMethod]
+		public void Test1()
+		{
+			var game = new MarbleGame();
+			game.PlaceNextMarble(1);
+			Assert.AreEqual("[1]  0 (1) ", game.ToString());
+		}
+	}
+}
---
+++ Day_09/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_09
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	public class MarbleGame : LinkedList<Marble>
+	{
+		public int NextMarbleNumber { get; set; }
+		public LinkedListNode<Marble> CurrentMarble { get; set; } = null;
+		public string LastPlayer { get; set; } = "-";
+
+		public MarbleGame()
+		{
+			NextMarbleNumber = 0;
+			CurrentMarble = AddFirst(new Marble(0, -1));
+		}
+
+		public void PlaceNextMarble(int player)
+		{
+			LastPlayer = player.ToString();
+			var neighbor = GetRightNeighborOf(CurrentMarble);
+			this.AddAfter(neighbor, new Marble(NextMarbleNumber, player));
+			NextMarbleNumber++;
+
+		}
+
+		// Clockwise
+		private LinkedListNode<Marble> GetRightNeighborOf(LinkedListNode<Marble> x)
+		{
+			return x.Next ?? this.First;
+		}
+
+		// Counter clockwise
+		private LinkedListNode<Marble> GetLeftNeighborOf(LinkedListNode<Marble> x)
+		{
+			return x.Previous ?? this.Last;
+		}
+
+		public override string ToString()
+		{
+			var items = string.Join(' ', this.Select(m => m.ToString(CurrentMarble.Value == m)));
+			return $"[{LastPlayer}] {items}";
+		}
+	}
+
+	public class Marble
+	{
+		public int Number { get; }
+		public int Player { get; }
+
+		public Marble(int number, int player)
+		{
+			Number = number;
+			Player = player;
+		}
+
+		public string ToString(bool parens = false)
+		{
+			return parens ? "(" + Number + ")" : Number.ToString("##") + " ";
+		}
+	}
+}
