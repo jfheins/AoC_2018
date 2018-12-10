bd89c0b8696646ffa8d628b6d06bea8f4622adfb
Julius F. Heins <jfheins@gmail.com>
2018-12-10 07:35:22 +0100

    Start Part 1

--- Day_10/Program.cs
+++ Day_10/Program.cs
@@ -3,21 +3,57 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace Day_10
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			var input = File.ReadAllLines(@"../../../input.txt");
+			var lights = input.Select(Light.FromLine).ToList();
 
-			for (int i = 0; i < rounds; i++)
+			var minSize = new Size(44000, 44000);
+
+			for (var i = 0; i < 100; i++)
 			{
-				game.PlaceNextMarble(i % players);
+				foreach (var light in lights)
+					light.Move();
+
+				var width = (long)lights.Max(l => l.Pos.X) - lights.Min(l => l.Pos.X) + 1;
+				var height = (long)lights.Max(l => l.Pos.Y) - lights.Min(l => l.Pos.Y) + 1;
+				if (width * height < minSize.Width * minSize.Height)
+				{
+					Debug.Assert(width < int.MaxValue);
+					minSize.Width = (int)width;
+					minSize.Height = (int)height;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			// Move back one sec
+			foreach (var light in lights)
+				light.Move(-1);
+
+			var minX = lights.Min(l => l.Pos.X);
+			var minY = lights.Min(l => l.Pos.Y);
+
+			for (var y = minY; y < minSize.Height; y++)
+			{
+				var line = new char[minSize.Width];
+				for (var x = minX; x < minSize.Width; x++)
+				{
+					if (lights.Any(l => l.IsAt(x, y)))
+						line[x-minX] = '#';
+					else
+						line[x - minX] = '.';
+				}
+
+				Console.WriteLine(line);
 			}
 
 			//Console.WriteLine(game.ToString());
@@ -25,22 +61,17 @@ namespace Day_10
 		}
 	}
 
-	class Light
+	internal class Light
 	{
+		private static readonly Regex regex = new Regex(@"position=<\s*([\-0-9]+),\s*([\-0-9]+)> velocity=<\s*([\-0-9]+),\s*([\-0-9]+)>");
+
 		public Point Pos { get; private set; }
 		public Size Velocity { get; private set; }
-
-		public Light()
-		{
-			
-		}
-
-		private static Regex regex = new Regex(@"position=<\s*(\d+),\s*(\d+)> velocity=<\s*(\d+),\s*(\d+)>");
 
 		public static Light FromLine(string s)
 		{
 			var groups = regex.Match(s).Groups;
-			var numbers = groups.Cast<string>().Skip(1).Select(int.Parse).ToArray();
+			var numbers = groups.Skip(1).Select(g => int.Parse(g.Value)).ToArray();
 			Debug.Assert(groups.Count == 5);
 			return new Light
 			{
@@ -48,6 +79,15 @@ namespace Day_10
 				Velocity = new Size(numbers[2], numbers[3])
 			};
 		}
+
+		public void Move(int seconds = 1)
+		{
+			Pos += Velocity * seconds;
+		}
+
+		public bool IsAt(int x, int y)
+		{
+			return Pos.X == x && Pos.Y == y;
+		}
 	}
 }
-
