611b081cab08a7da88537471c1a3584f6c0c2c54
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-11 14:52:36 +0100

    Code cleanup/refactor

--- Day_11/Program.cs
+++ Day_11/Program.cs
@@ -1,73 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Core;
 using MoreLinq;
 
 namespace Day_11
 {
-	internal class Program
-	{
-		private static int _serialNumber;
+    internal class Program
+    {
+        private static int _serialNumber;
 
-		public static void Main(string[] args)
-		{
-			_serialNumber = 7315;
-			var gridsize = 300;
+        public static void Main(string[] args)
+        {
+            _serialNumber = 7315;
+            var gridsize = 300;
 
-			var sw = new Stopwatch();
-			sw.Start();
+            var sw = new Stopwatch();
+            sw.Start();
 
-			var powers = new Dictionary<(int, int, int), int>();
-			
-			for (var sqsize = 1; sqsize <= 20; sqsize++)
-			{
-				Console.WriteLine($"Square: {sqsize}");
-				for (var x = 0; x < gridsize - sqsize + 1; x++)
-				for (var y = 0; y < gridsize - sqsize + 1; y++)
-				{
-					var power = 0;
-					for (var i = 1; i <= sqsize; i++)
-					for (var j = 1; j <= sqsize; j++)
-					{
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-						power += GetPowerLevel(x + i, y + j);
-					}
+            var powers = new Dictionary<(int, int, int), int>();
 
-					powers[(x + 1, y + 1, sqsize)] = power;
-				}
-			}
+            for (var sqsize = 1; sqsize <= 20; sqsize++)
+            {
+                Console.WriteLine($"Squaresize: {sqsize}");
+                for (var x = 0; x < gridsize - sqsize + 1; x++)
+                {
+                    for (var y = 0; y < gridsize - sqsize + 1; y++)
+                    {
+                        powers[(x + 1, y + 1, sqsize)] = CalcPowerLevelForSquare(x, y, sqsize);
+                    }
+                }
+            }
 
-			var max = powers.MaxBy(kvp => kvp.Value).First();
+            var max = powers.MaxBy(kvp => kvp.Value).First();
 
-			Console.WriteLine(max.Key);
+            Console.WriteLine(max.Key);
 
-			sw.Stop();
-			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
 
-			Console.ReadLine();
-		}
+            Console.ReadLine();
+        }
 
-		//private static int Get2PowerLevel(int x, int y)
-		//{
-		//	return cache.GetOrAdd((x, y), () => CalcPowerLevel(x, y));
-		//}
+        private static int CalcPowerLevelForSquare(int x, int y, int size)
+        {
+            var power = 0;
+            for (var i = 1; i <= size; i++)
+            {
+                for (var j = 1; j <= size; j++)
+                {
+                    power += GetPowerLevelForCell(x + i, y + j);
+                }
+            }
 
-		private static int GetPowerLevel(int x, int y)
-		{
-			var rackid = x + 10;
-			var power = rackid * y;
-			power += _serialNumber;
-			power *= rackid;
-			power = power % 1000 / 100;
-			return power - 5;
-		}
-	}
+            return power;
+        }
+
+        private static int GetPowerLevelForCell(int x, int y)
+        {
+            var rackid = x + 10;
+            var power = rackid * y;
+            power += _serialNumber;
+            power *= rackid;
+            power = power % 1000 / 100;
+            return power - 5;
+        }
+    }
 }
