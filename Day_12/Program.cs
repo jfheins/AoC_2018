1432ba8a0ed5addbc061bbd1addc9efe929c31be
Julius Heins <Julius.JH.Heins@bmw.de>
2018-12-11 15:48:26 +0100

    Prep

---
+++ Day_12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText(@"../../../input.txt");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            Console.ReadLine();
+        }
+    }
+}
