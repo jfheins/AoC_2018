74140262f15f9e9cd80c2662812396b440815d3a
Julius F. Heins <jfheins@gmail.com>
2018-12-13 06:26:56 +0100

    Part 1

--- Day_13/Program.cs
+++ Day_13/Program.cs
@@ -1,26 +1,141 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using MoreLinq.Extensions;
+using Core;
+using Core.Combinatorics;
 
 namespace Day_13
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			var input = File.ReadAllLines(@"../../../input.txt");
-
+			var input = File.ReadAllLines(@"../../../demo.txt");
 			var sw = new Stopwatch();
 			sw.Start();
 
+			var tracks = input.Select(ParseLineToTrack).ToArray();
+			var carts = input.SelectMany(ParseCarts).ToArray();
 
+			for (var tick = 1; tick < 10; tick++)
+			{
+				foreach (var cart in carts.OrderBy(c => c.Position.X).ThenBy(c => c.Position.Y))
+				{
+					cart.Move();
+					foreach (var other in carts.Except(cart.ToEnumerable()).Where(other => cart.Position == other.Position))
+					{
+						cart.IsBroken = true;
+						other.IsBroken = true;
+						Console.WriteLine($"Crash at {cart.Position}");
+					}
+				}
+
+				// Move around corners
+				foreach (var cart in carts)
+				{
+					var trackSymbol = tracks[cart.Position.Y][cart.Position.X];
+						cart.Turn(trackSymbol);
+					
+				}
+			}
 
 			sw.Stop();
 			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
 			Console.ReadLine();
+		}
+
+		private static IEnumerable<Cart> ParseCarts(string line, int y)
+		{
+			for (var i = 0; i < line.Length; i++)
+			{
+				if ("<^>v".Contains(line[0]))
+				{
+					yield return new Cart(i, y, line[0]);
+				}
+			}
+		}
+
+		private static string ParseLineToTrack(string arg)
+		{
+			return arg.Replace('<', '-')
+				.Replace('^', '|')
+				.Replace('>', '-')
+				.Replace('v', '|');
+		}
+
+		public class Cart
+		{
+			public Cart(int x, int y, char v)
+			{
+				Position = new Point(x, y);
+				Symbol = v;
+			}
+
+			public Point Position { get; set; }
+			public char Symbol { get; set; }
+			public bool IsBroken { get; set; }
+
+			public void Move()
+			{
+				switch (Symbol)
+				{
+					case '<':
+						Position = new Point(Position.X - 1, Position.Y);
+						return;
+					case '^':
+						Position = new Point(Position.X, Position.Y - 1);
+						return;
+					case '>':
+						Position = new Point(Position.X + 1, Position.Y);
+						return;
+					case 'v':
+						Position = new Point(Position.X, Position.Y + 1);
+						return;
+				}
+			}
+
+			public void Turn(char trackSymbol)
+			{
+				if (trackSymbol == '/')
+				{
+					switch (Symbol)
+					{
+						case '<':
+							Symbol = 'v';
+							return;
+						case '^':
+							Symbol = '>';
+							return;
+						case '>':
+							Symbol = '^';
+							return;
+						case 'v':
+							Symbol = '<';
+							return;
+					}
+				}
+				if (trackSymbol == '\\')
+				{
+					switch (Symbol)
+					{
+						case '<':
+							Symbol = '^';
+							return;
+						case '^':
+							Symbol = '<';
+							return;
+						case '>':
+							Symbol = 'v';
+							return;
+						case 'v':
+							Symbol = '>';
+							return;
+					}
+				}
+			}
 		}
 	}
 }
