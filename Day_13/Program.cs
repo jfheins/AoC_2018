64b22b4f8067f798e461e2f8a0c16296129c33f5
Julius F. Heins <jfheins@gmail.com>
2018-12-13 06:00:02 +0100

    Prep

---
+++ Day_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using MoreLinq.Extensions;
+
+namespace Day_13
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = File.ReadAllLines(@"../../../input.txt");
+
+			var sw = new Stopwatch();
+			sw.Start();
+
+
+
+			sw.Stop();
+			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+			Console.ReadLine();
+		}
+	}
+}
