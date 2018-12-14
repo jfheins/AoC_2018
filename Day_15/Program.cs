2586e185f5e638a4fdf5db36aaf974ba383ba59a
Julius F. Heins <jfheins@gmail.com>
2018-12-14 12:21:40 +0100

    Prep

---
+++ Day_15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day_15
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = File.ReadAllLines(@"../../../input.txt");
+			var sw = new Stopwatch();
+			sw.Start();
+
+
+
+			Console.WriteLine($"Part 1: ");
+			Console.WriteLine($"Part 2: ");
+
+			sw.Stop();
+			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+			Console.ReadLine();
+		}
+	}
+}
