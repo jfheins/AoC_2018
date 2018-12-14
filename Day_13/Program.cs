4e981fc7ea0a410306234227f0b67eafbd39ae3e
Julius F. Heins <jfheins@gmail.com>
2018-12-14 11:48:06 +0100

    Add nice visualization

--- Day_13/Program.cs
+++ Day_13/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
@@ -9,6 +11,15 @@ namespace Day_13
 {
 	internal class Program
 	{
+		private static Bitmap _trackMap;
+
+		private static readonly Size offset = new Size(2, 2);
+		private static readonly Size cartSize = new Size(1, 1);
+		private static readonly Size unitSize = new Size(1, 1);
+		private static readonly int scale = 3;
+
+		private static readonly Pen _pathPen = new Pen(Color.Green);
+
 		private static void Main()
 		{
 			var input = File.ReadAllLines(@"../../../input.txt");
@@ -16,13 +27,22 @@ namespace Day_13
 			sw.Start();
 
 			var sim = new TrackSimulator(input);
+			GenerateTrackMap(sim);
 
 			// initial
 			Console.WriteLine($"{sim.Carts.Count} carts found on map.");
 
 			while (sim.Carts.Count > 1)
 			{
+				var pic = PaintPicture(sim);
+				pic.Save(@"E:\Temp\aoc\" + sim.tick.ToString("D5") + ".bmp", ImageFormat.Bmp);
+				pic.Dispose();
 				sim.Step();
+
+				if (sim.tick > 5000)
+				{
+					break;
+				}
 			}
 
 			Console.WriteLine($"First crash was at {sim.crashLocations.First().location}");
@@ -34,6 +54,70 @@ namespace Day_13
 			sw.Stop();
 			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
 			Console.ReadLine();
+		}
+
+		private static void GenerateTrackMap(TrackSimulator sim)
+		{
+			_trackMap = new Bitmap(scale * sim.Width + offset.Width * 2, scale * sim.Height + offset.Height * 2);
+
+			using (var graphics = Graphics.FromImage(_trackMap))
+			{
+				// Paint tracks
+				for (var x = 0; x < sim.Width; x++)
+				{
+					for (var y = 0; y < sim.Height; y++)
+					{
+						var chr = sim._tracks[y][x];
+						if ("-|/\\".Contains(chr))
+						{
+							_trackMap.SetPixel(scale * x + offset.Width, scale * y + offset.Height, Color.DarkGray);
+						}
+					}
+				}
+			}
+		}
+
+		private static Bitmap PaintPicture(TrackSimulator sim)
+		{
+			var bmp = new Bitmap(scale * sim.Width + offset.Width * 2, scale * sim.Height + offset.Height * 2);
+
+			Point Scale(Point p)
+			{
+				return new Point(scale * p.X, scale * p.Y);
+			}
+			using (var graphics = Graphics.FromImage(bmp))
+			{
+				graphics.FillRectangle(Brushes.Black, graphics.ClipBounds);
+				graphics.CompositingQuality = CompositingQuality.HighSpeed;
+				// Paint tracks
+				graphics.DrawImageUnscaled(_trackMap, 0, 0);
+
+				// Paint traces
+				foreach (var path in sim.Carts.Select(c => c.Path))
+				{
+					if (path.Count > 1)
+					{
+						var points = path.TakeLast(50).Select(Scale);
+						graphics.DrawLines(_pathPen, points.ToArray());
+					}
+				}
+
+				// Paint carts
+				foreach (var cart in sim.Carts)
+				{
+					var cartRect = new Rectangle(Scale(cart.Position) - cartSize, cartSize * 2 + unitSize);
+					graphics.FillRectangle(Brushes.Aqua, cartRect);
+				}
+
+				// Paint crashes
+				foreach (var p in sim.crashLocations)
+				{
+					var crashRect = new Rectangle(Scale(p.location) - cartSize, cartSize * 2 + unitSize);
+					graphics.FillRectangle(Brushes.Red, crashRect);
+				}
+			}
+
+			return bmp;
 		}
 	}
 
@@ -47,15 +131,20 @@ namespace Day_13
 			Down
 		}
 
-		private readonly string[] _tracks;
+		public readonly string[] _tracks;
 		public readonly List<Cart> Carts;
-		private int tick;
+		public int tick;
 
 		public TrackSimulator(string[] input)
 		{
 			_tracks = input.Select(ParseLineToTrack).ToArray();
 			Carts = ParseCarts(input).ToList();
+			Width = _tracks[0].Length;
+			Height = _tracks.Length;
 		}
+
+		public int Width { get; }
+		public int Height { get; }
 
 		public ICollection<(int tick, Point location)> crashLocations { get; } = new List<(int, Point)>();
 
@@ -142,6 +231,7 @@ namespace Day_13
 			public Cart(int x, int y, char symbol)
 			{
 				Position = new Point(x, y);
+				Path.Add(Position);
 				MovingDirection = DirectionFromSymbol(symbol);
 				TurnCounter = -1;
 			}
@@ -149,6 +239,7 @@ namespace Day_13
 			public int TurnCounter { get; private set; }
 			public Point Position { get; private set; }
 			public Direction MovingDirection { get; private set; }
+			public List<Point> Path { get; } = new List<Point>();
 
 			public Size Velocity => _mapDirectionToSize[MovingDirection];
 
@@ -160,6 +251,7 @@ namespace Day_13
 			public void Move()
 			{
 				Position += Velocity;
+				Path.Add(Position);
 			}
 
 			private void TurnLeft()
