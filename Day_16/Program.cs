3c2564f2c373b1c81e64efd71aed5abb69e0d45b
Julius F. Heins <jfheins@gmail.com>
2018-12-16 07:48:01 +0100

    First code

--- Day_16/Program.cs
+++ Day_16/Program.cs
@@ -3,25 +3,173 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Core;
 
 namespace Day_16
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			var input = File.ReadAllLines(@"../../../input.txt");
+			var input = File.ReadAllLines(@"../../../input_1.txt");
 			var sw = new Stopwatch();
 			sw.Start();
 
+			var numbers = input.Select(l => l.ParseInts()).Chunks(4);
+			var beforeAfter = numbers.Select(chunk =>
+			{
+				var block = chunk.ToArray();
+				return new
+				{
+					before = block[0],
+					opcode = block[1],
+					after = block[2]
+				};
+			});
+
+			var threeormore = 0;
+			//foreach (var sample in beforeAfter)
+			{
+				var sample = new
+				{
+					before = new[] { 3, 2, 1, 1},
+					opcode = new[] { 9, 2, 1, 2  },
+					after = new[] { 3, 2, 2, 1 }
+				};
+
+				var possibleOpcodes = new List<OpCode>();
+				for (int opcode = 0; opcode < 15; opcode++)
+				{
+					var instruction = InstructionFromOpcodeAndContent(opcode, sample.opcode);
+					var result = Calculate(instruction, sample.before);
+					if (result.SequenceEqual(sample.after))
+					{
+						possibleOpcodes.Add(instruction.OpCode);
+					}
+				}
+
+				if (possibleOpcodes.Count >= 3)
+				{
+					threeormore++;
+				}
+			}
 
 
-			Console.WriteLine($"Part 1: ");
-			Console.WriteLine($"Part 2: ");
+			Console.WriteLine($"Part 1: {threeormore}");
+			Console.WriteLine("Part 2: ");
 
 			sw.Stop();
 			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
 			Console.ReadLine();
 		}
+
+		public static Instruction InstructionFromOpcodeAndContent(int opcode, int[] content)
+		{
+			return new Instruction((OpCode)opcode, content[1], content[2], content[3]);
+		}
+
+		public static Instruction InstructionFromInput(int[] content)
+		{
+			return new Instruction((OpCode)content[0], content[1], content[2], content[3]);
+		}
+
+		public static int[] Calculate(Instruction op, int[] registers)
+		{
+			var result = registers.ToArray();
+			switch (op.OpCode)
+			{
+				case OpCode.Addr:
+					result[op.C] = result[op.A] + result[op.B];
+					break;
+				case OpCode.Addi:
+					result[op.C] = result[op.A] + op.B;
+					break;
+				case OpCode.Mulr:
+					result[op.C] = result[op.A] * result[op.B];
+					break;
+				case OpCode.Muli:
+					result[op.C] = result[op.A] * op.B;
+					break;
+				case OpCode.Banr:
+					result[op.C] = result[op.A] & result[op.B];
+					break;
+				case OpCode.Bani:
+					result[op.C] = result[op.A] & op.B;
+					break;
+				case OpCode.Borr:
+					result[op.C] = result[op.A] | result[op.B];
+					break;
+				case OpCode.Bori:
+					result[op.C] = result[op.A] | op.B;
+					break;
+				case OpCode.Setr:
+					result[op.C] = result[op.A];
+					break;
+				case OpCode.Seti:
+					result[op.C] = op.A;
+					break;
+				case OpCode.Gtir:
+					result[op.C] = op.A > result[op.B] ? 1 : 0;
+					break;
+				case OpCode.Gtri:
+					result[op.C] = result[op.A] > op.B ? 1 : 0;
+					break;
+				case OpCode.Gtrr:
+					result[op.C] = result[op.A] > result[op.B] ? 1 : 0;
+					break;
+				case OpCode.Eqir:
+					result[op.C] = op.A == result[op.B] ? 1 : 0;
+					break;
+				case OpCode.Eqri:
+					result[op.C] = result[op.A] == op.B ? 1 : 0;
+					break;
+				case OpCode.Eqrr:
+					result[op.C] = result[op.A] == result[op.B] ? 1 : 0;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(op), op, null);
+			}
+
+			return result;
+		}
+	}
+
+	public struct Instruction
+	{
+		public OpCode OpCode { get; }
+		public int A { get; }
+		public int B { get; }
+		public int C { get; }
+
+		public Instruction(OpCode opCode,
+						   int a,
+						   int b,
+						   int c)
+		{
+			OpCode = opCode;
+			A = a;
+			B = b;
+			C = c;
+		}
+	}
+
+	public enum OpCode
+	{
+		Addr,
+		Addi,
+		Mulr,
+		Muli,
+		Banr,
+		Bani,
+		Borr,
+		Bori,
+		Setr,
+		Seti,
+		Gtir,
+		Gtri,
+		Gtrr,
+		Eqir,
+		Eqri,
+		Eqrr
 	}
 }
