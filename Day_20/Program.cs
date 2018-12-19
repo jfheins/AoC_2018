7b7e85b51cdcb0ec468686ef64fac0119703980a
Julius F. Heins <jfheins@gmail.com>
2018-12-19 21:59:19 +0100

    Prep

---
+++ Day_20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day_20
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = File.ReadAllLines(@"../../../input.txt");
+			var sw = new Stopwatch();
+			sw.Start();
+
+
+
+			Console.WriteLine($"Part 1: ");
+			Console.WriteLine($"Part 2: ");
+
+			sw.Stop();
+			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+			Console.ReadLine();
+		}
+	}
+}
