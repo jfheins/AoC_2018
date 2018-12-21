cfc9e0c1e8ec4b4777a50310f6a96015e035ef49
Julius F. Heins <jfheins@gmail.com>
2018-12-21 14:10:35 +0100

    Failed first attempt

--- Day_20/Program.cs
+++ Day_20/Program.cs
@@ -1,25 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day_20
 {
 	public class Program
 	{
-		static void Main(string[] args)
+		private static readonly Regex groupMatcher = new Regex(@"(.+)\(([NEWS|]+)\)(.+)");
+
+		private static void Main(string[] args)
 		{
-			var input = File.ReadAllLines(@"../../../input.txt");
+			var input = File.ReadAllText(@"../../../input.txt");
+			//input = @"^E|SSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
 			var sw = new Stopwatch();
 			sw.Start();
 
+			var possiblePaths = new HashSet<string>(RegexToPath(input));
+
+			while (didWork)
+			{
+				didWork = false;
+				possiblePaths = new HashSet<string>(possiblePaths.SelectMany(RegexToPath));
+			}
+			// Top level option?
+			Debug.Assert(possiblePaths.All(s => !s.Contains('|')), "No path shall contain |");
 
 
-			Console.WriteLine($"Part 1: ");
-			Console.WriteLine($"Part 2: ");
+
+			Console.WriteLine(possiblePaths.Count);
+
+			Console.WriteLine("Part 1: ");
+			Console.WriteLine("Part 2: ");
 
 			sw.Stop();
 			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
 			Console.ReadLine();
+		}
+
+		private static bool didWork = true;
+
+		private static IEnumerable<string> RegexToPath(string str)
+		{
+			var match = groupMatcher.Match(str);
+
+			if (match.Success)
+			{
+				didWork = true;
+				Debug.Assert(match.Groups.Count == 4);
+
+				var options = ParseOptions(match.Groups[2].Value);
+
+				foreach (var option in options)
+					yield return match.Groups[1].Value + option + match.Groups[3].Value;
+			}
+			else
+			{
+				yield return str;
+			}
+		}
+
+		private static IEnumerable<string> ParseOptions(string str)
+		{
+			return str.Split('|');
+		}
+	}
+
+	public class RegexComponent { }
+
+
+	public class Literal : RegexComponent
+	{
+		public Literal(ReadOnlySpan<char> value)
+		{
+			Value = value.ToString();
+		}
+
+		public Literal()
+		{
+			Value = "";
+		}
+
+		public string Value { get; set; }
+	}
+
+	public class Alternatives : RegexComponent
+	{
+		public List<RegexComponent> Parts { get; } = new List<RegexComponent>();
+	}
+
+	public class Sequence : RegexComponent
+	{
+		public List<RegexComponent> Parts { get; } = new List<RegexComponent>();
+
+		public void Add(RegexComponent item)
+		{
+			Parts.Add(item);
 		}
 	}
 }
