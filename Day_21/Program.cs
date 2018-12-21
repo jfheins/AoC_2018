b55a3d76e905ce0f0e58fa48c5acfe6f40b7e50b
Julius F. Heins <jfheins@gmail.com>
2018-12-21 12:16:59 +0100

    Solve Day 21 Part 1

---
+++ Day_21/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Core;
+using Day_16;
+
+namespace Day_21
+{
+	internal class Program
+	{
+		private static int instructionCounter;
+
+		private static void Main(string[] args)
+		{
+			var input = File.ReadAllLines(@"../../../input.txt");
+			var sw = new Stopwatch();
+			sw.Start();
+
+			var instructions = input.Skip(1).Select(ParseLine).ToArray();
+
+			var ipRegister = int.Parse(input[0][4].ToString());
+			var ip = 0;
+
+			var registers = new[] { 7224964, 0, 0, 0, 0, 0};
+
+
+			while (ip >= 0 && ip < instructions.Length)
+			{
+				if (ip == 13 && registers[4] < 256)
+				{
+					Console.WriteLine(registers[5]);
+				}
+
+				if (ip >= 18 && ip <= 25)
+				{
+					ip = InnerLoop(ref registers);
+					//Console.WriteLine(registers[5]);
+				}
+				else
+				{
+					registers[ipRegister] = ip;
+					Calculate(instructions[ip], ref registers);
+					ip = registers[ipRegister];
+				}
+				ip++;
+			}
+
+			Console.WriteLine($"Part 1: {instructionCounter}");
+			Console.WriteLine("Part 2: ");
+
+			sw.Stop();
+			Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+			Console.ReadLine();
+		}
+
+
+		private static int InnerLoop(ref int[] registers)
+		{
+			registers[3] = registers[4] / 256;
+			registers[2] = registers[3] + 1;
+
+			registers[1] = 25;
+			return 25;
+		}
+
+		private static Instruction ParseLine(string line)
+		{
+			var str = line.Substring(0, 4);
+			var opcode = Enum.Parse<OpCode>(str, true);
+			var args = line.ParseInts();
+			return new Instruction(opcode, args[0], args[1], args[2]);
+		}
+
+		private static void Calculate(Instruction op, ref int[] registers)
+		{
+			instructionCounter++;
+			switch (op.OpCode)
+			{
+				case OpCode.Addr:
+					registers[op.C] = registers[op.A] + registers[op.B];
+					break;
+				case OpCode.Addi:
+					registers[op.C] = registers[op.A] + op.B;
+					break;
+				case OpCode.Mulr:
+					registers[op.C] = registers[op.A] * registers[op.B];
+					break;
+				case OpCode.Muli:
+					registers[op.C] = registers[op.A] * op.B;
+					break;
+				case OpCode.Banr:
+					registers[op.C] = registers[op.A] & registers[op.B];
+					break;
+				case OpCode.Bani:
+					registers[op.C] = registers[op.A] & op.B;
+					break;
+				case OpCode.Borr:
+					registers[op.C] = registers[op.A] | registers[op.B];
+					break;
+				case OpCode.Bori:
+					registers[op.C] = registers[op.A] | op.B;
+					break;
+				case OpCode.Setr:
+					registers[op.C] = registers[op.A];
+					break;
+				case OpCode.Seti:
+					registers[op.C] = op.A;
+					break;
+				case OpCode.Gtir:
+					registers[op.C] = op.A > registers[op.B] ? 1 : 0;
+					break;
+				case OpCode.Gtri:
+					registers[op.C] = registers[op.A] > op.B ? 1 : 0;
+					break;
+				case OpCode.Gtrr:
+					registers[op.C] = registers[op.A] > registers[op.B] ? 1 : 0;
+					break;
+				case OpCode.Eqir:
+					registers[op.C] = op.A == registers[op.B] ? 1 : 0;
+					break;
+				case OpCode.Eqri:
+					registers[op.C] = registers[op.A] == op.B ? 1 : 0;
+					break;
+				case OpCode.Eqrr:
+					registers[op.C] = registers[op.A] == registers[op.B] ? 1 : 0;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(op), op, null);
+			}
+		}
+	}
+}
