fa027260935cef1d627ec6c6c5b8ab2efd253691
Julius Heins <jfheins@gmail.com>
2018-12-23 19:49:41 +0100

    Prep

---
+++ Day_24/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"../../../input.txt");
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+
+            Console.WriteLine($"Part 1: Point");
+            Console.WriteLine($"Part 2: x");
+
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            Console.ReadLine();
+        }
+    }
+}
